8034b71b90c10561a0e4e7a23f079216e429950e
irusics <i.rusics@samsung.com>
2018-12-21 13:27:46 +0100

    some files added

---
+++ _set_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Just a Test
+############################################
+STRING="_set_env.sh"
+#print variable on a screen
+echo $STRING
+############################################
