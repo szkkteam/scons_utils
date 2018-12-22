3069df12dc2eeebfa1ed48c5c4ed8836f6cca27b
Istvan Rusics <szkkteam1@gmail.com>
2018-12-22 08:39:20 +0100

    Scripts are moved to bash_repo git repository.

--- _set_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Just a Test
-############################################
-STRING="_set_env.sh"
-#print variable on a screen
-echo $STRING
-############################################
