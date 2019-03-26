b29e9ce8b92e0547a041ccf056317f44bdbe6cc7
Istvan Rusics <szkkteam1@gmail.com>
2019-03-26 21:11:16 +0100

    Options added, enviroments updated

--- enviroment.py
+++ enviroment.py
@@ -6,5 +6,126 @@ import os
 import sys
 import platform
 import re
+from   SCons.Script import *
 
 
+# Map of build host to possible target os
+host_target_map = {
+    'linux': ['linux'],
+    'windows': ['windows']
+    #TODO: support more pre-build target.
+}
+
+# Map of target os to possible target architecture
+os_arch_map = {
+    'linux': [
+        'x86', 'x86_64', 'arm', 'armv7l', 'arm-v7a', 'armeabi-v7a', 'arm64', 'mips',
+        'mipsel', 'mips64', 'mips64el', 'i386', 'powerpc', 'sparc', 'aarch64',
+        'armv6l', 'armv7l'
+    ],
+    'windows': ['x86', 'amd64', 'arm']
+}
+
+######################################################################
+# Platform (build target) specific options: SDK/NDK & toolchain
+######################################################################
+targets_support_cc = ['linux', 'windows']
+
+def GetHost():
+    # Fetch host from Python platform information and smash case
+    host = platform.system().lower()
+
+    if host not in host_target_map:
+        msg = "\nError: building on host os '%s' is not currently supported.\n" % host
+        Exit(msg)
+    return host
+
+def GetDefaultArch(target_os):
+    # work out a reasonable default for target_arch if not specified by user
+    default_arch = platform.machine()
+    if target_os == 'windows':
+        default_arch = default_arch.lower()
+    if target_os == 'linux' and default_arch in os_arch_map[target_os]:
+        default_arch = default_arch.lower()
+
+    return default_arch
+
+######################################################################
+# this is where the setup of the construction envionment begins
+######################################################################
+def GetDefaultEnvironment(help_vars,target_os, target_arch):
+    env = Environment(
+        variables=help_vars,
+        tools=['default', 'textfile'],
+        TARGET_ARCH=target_arch,
+        TARGET_OS=target_os,
+        #PREFIX=GetOption('prefix'),
+        LIB_INSTALL_DIR=ARGUMENTS.get('LIB_INSTALL_DIR')  #for 64bit build
+    )
+
+    if env.get('WITH_ENV'):
+        env['ENV'] = os.environ
+        if 'CC' in os.environ:
+            env['CC'] = Split(os.environ['CC'])
+            print("using CC from environment: %s" % env['CC'])
+        if 'CXX' in os.environ:
+            env['CXX'] = Split(os.environ['CXX'])
+            print("using CXX from environment: %s" % env['CXX'])
+        if 'CFLAGS' in os.environ:
+            env['CFLAGS'] = Split(os.environ['CFLAGS'])
+            print("using CFLAGS from environment: %s" % env['CFLAGS'])
+        if 'CXXFLAGS' in os.environ:
+            env['CXXFLAGS'] = Split(os.environ['CXXFLAGS'])
+            print("using CXXFLAGS from environment: %s" % env['CXXFLAGS'])
+        if 'CCFLAGS' in os.environ:
+            env['CCFLAGS'] = Split(os.environ['CCFLAGS'])
+            print("using CCFLAGS from environment: %s" % env['CCFLAGS'])
+        if 'CPPFLAGS' in os.environ:
+            env['CPPFLAGS'] = Split(os.environ['CPPFLAGS'])
+            print("using CPPFLAGS from environment: %s" % env['CPPFLAGS'])
+        if 'LDFLAGS' in os.environ:
+            env['LINKFLAGS'] = Split(os.environ['LDFLAGS'])
+            print("using LDFLAGS/LINKFLAGS from environment: %s" % env['LINKFLAGS'])
+
+    # set quieter build messages unless verbose mode was requested
+    if not env.get('VERBOSE'):
+        env['CCCOMSTR'] = "Compiling $TARGET"
+        env['SHCCCOMSTR'] = "Compiling $TARGET"
+        env['CXXCOMSTR'] = "Compiling $TARGET"
+        env['SHCXXCOMSTR'] = "Compiling $TARGET"
+        env['LINKCOMSTR'] = "Linking $TARGET"
+        env['SHLINKCOMSTR'] = "Linking shared object $TARGET"
+        env['ARCOMSTR'] = "Archiving $TARGET"
+        env['RANLIBCOMSTR'] = "Indexing Archive $TARGET"
+
+    tc_set_msg = '''
+************************************ Warning **********************************
+* Warning: TC_PREFIX and/or TC_PATH is set in the environment.
+* This means a non-default compilation toolchain will be used.
+* If this is not what you expected you should unset, or it
+* may lead to unexpected results.
+*******************************************************************************
+    '''
+    if target_os in targets_support_cc:
+        prefix = env.get('TC_PREFIX')
+        tc_path = env.get('TC_PATH')
+        if prefix:
+            env.Replace(CC=prefix + env.get('CC', 'gcc'))
+            env.Replace(CXX=prefix + env.get('CXX', 'g++'))
+            env.Replace(AR=prefix + env.get('AR', 'ar'))
+            env.Replace(AS=prefix + env.get('AS', 'as'))
+            env.Replace(RANLIB=prefix + env.get('RANLIB', 'ranlib'))
+
+        if tc_path:
+            env.PrependENVPath('PATH', tc_path)
+            sys_root = os.path.abspath(tc_path + '/../')
+            env.AppendUnique(CCFLAGS=['--sysroot=' + sys_root])
+            env.AppendUnique(LINKFLAGS=['--sysroot=' + sys_root])
+
+        if prefix or tc_path:
+            print(tc_set_msg)
+
+    # Ensure scons is able to change its working directory
+    env.SConscriptChdir(1)
+
+    return env
--- options.py
+++ options.py
@@ -8,17 +8,135 @@ import platform
 import re
 #import SCons.Variables
 from   SCons.Script import *
+from enviroment import *
 
 ######################################################################
 # Common build options
 ######################################################################
 
+def GetTargetOs():
+    host = GetHost()
+    target_os = ARGUMENTS.get('TARGET_OS', host).lower()
+
+    if target_os not in host_target_map[host]:
+        msg = "\nError: host '%s' cannot currently build target '%s'" % (host, target_os)
+        msg += "\n\tchoices: %s\n" % host_target_map[host]
+        Exit(msg)
+
+    return target_os
+
+def GetTargetArch(target_os):
+    target_arch = ARGUMENTS.get('TARGET_ARCH', GetDefaultArch(target_os))  # target arch
+    if target_arch not in os_arch_map[target_os]:
+        msg = "\nError: target os '%s' cannot currently build target '%s'" % (target_os, target_arch)
+        msg += "\n\tchoices: %s\n" % os_arch_map[target_os]
+        Exit(msg)
+
+    return target_arch
+
 def InitializeCommandLineOptions(project_version):
+
+    host = GetHost()
+
+    target_os = GetTargetOs()
+
+    # generate a list of unique targets: convert to set() for uniqueness,
+    # then convert back to a list
+    targetlist = list(set(x for l in list(host_target_map.values()) for x in l))
+
+    default_arch = GetDefaultArch(target_os)
+
+    if ARGUMENTS.get('RUN_TEST') == 'ON':
+        logging_default = False
+    else:
+        release_mode = False
+        if ARGUMENTS.get('RELEASE', True) in [
+            'y', 'yes', 'true', 't', '1', 'on', 'all', True
+        ]:
+            release_mode = True
+        logging_default = (release_mode is False)
+
     help_vars = Variables()
     help_vars.AddVariables(
-        ('PROJECT_VERSION',
+                ('PROJECT_VERSION',
                      'The version of Scons Utils',
                      project_version),
-     )
-     
+    BoolVariable('VERBOSE',
+                 'Show compilation',
+                 default=False),
+    BoolVariable('RELEASE',
+                 'Build for release?',
+                 default=True),
+    EnumVariable('TARGET_OS',
+                 'Target platform',
+                 default=host,
+                 allowed_values=targetlist),
+    EnumVariable('BUILD_TESTS',
+                 'Build unit tests',
+                 default='ON',
+                 allowed_values=('ON', 'OFF')),
+    EnumVariable('TARGET_ARCH',
+                 'Target architecture',
+                 default=default_arch,
+                 allowed_values=os_arch_map[target_os]),
+    EnumVariable('RUN_TEST',
+                 'Run unit tests',
+                 default='ON',
+                 allowed_values=('ON', 'OFF')),
+    BoolVariable('LOGGING',
+                 'Enable stack logging',
+                 default=logging_default),
+    EnumVariable('LOG_LEVEL',
+                 'Enable stack logging level',
+                 default='DEBUG',
+                 allowed_values=('DEBUG', 'INFO', 'ERROR', 'WARNING', 'FATAL')),
+    EnumVariable('BUILD_SAMPLE',
+                 'Build with sample',
+                 default='ON',
+                 allowed_values=('ON', 'OFF')),
+    BoolVariable('WITH_ENV',
+                 'Use compiler options from environment',
+                 default=False),
+
+    )
+
+    ######################################################################
+    # Platform (build target) specific options
+    ######################################################################
+    if target_os in ['linux']:
+        # Build option to enable failing build if warnings encountered.
+        # May need to be off for developing with newer compilers which
+        # are stricter about emitting warnings. Defaults to true so
+        # developer builds and gerrit builds have all warnings examined.
+        help_vars.Add(
+            BoolVariable('ERROR_ON_WARN',
+                         'Make all compiler warnings into errors.',
+                         default=False))
+
+    if target_os == 'windows':
+        # Builds differ based on Visual Studio version
+        #   For VS2013, MSVC_VERSION is '12.0'.
+        #   For VS2015, MSVC_VERSION is '14.0'.
+        #   For VS2017, MSVC_VERSION is '14.1'.
+        # Default value is None, which means SCons will pick
+        help_vars.Add(
+            EnumVariable('MSVC_VERSION',
+                         'MSVC compiler version - Windows',
+                         default=None,
+                         allowed_values=('12.0', '14.0', '14.1')))
+
+    ######################################################################
+    # Platform (build target) specific options: SDK/NDK & toolchain
+    ######################################################################
+    if target_os in targets_support_cc:
+        # Set cross compile toolchain
+        help_vars.Add('TC_PREFIX',
+                      "Toolchain prefix (Generally only required for cross-compiling)",
+                      default=None)
+        help_vars.Add(
+            PathVariable('TC_PATH',
+                         'Toolchain path (Generally only required for cross-compiling)',
+                         default=None,
+                         validator=PathVariable.PathAccept))
+
     return help_vars
